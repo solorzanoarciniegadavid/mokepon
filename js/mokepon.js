0e42191febde09c18907b69af70915294ddcc016
David Solorzano <sdavid@grupocva.com>
2022-08-11 16:31:47 -0500

    Nuevo update en js

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,3 +1,10 @@
 alert("Mokepon");
 
+
+function seleccionarMascotaJugador(){
+    alert("Seleccionaste tu mascota");
+}
+
+
 let btnMascotaJugador = document.getElementById("btn-mascota");
+btnMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
